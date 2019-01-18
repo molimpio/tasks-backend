060586206a50296faa6ab840ce951edf53dbf9df
Moises <moises@rcbdesenvolvimento.com.br>
2019-01-18 07:26:08 -0200

    configurando middlewares

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
-app.get('/', (req, res) => {
-    res.status(200).send('Backend')
-})
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
+
+app.db = db
 
 app.listen(3000, () => console.log('backend rodando na porta 3000'))
